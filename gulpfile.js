0301e1ca93d34b915f4473fc760df89261448608
Chris Wheatley <chriswheatley87@gmail.com>
2015-05-25 15:07:26 +0100

    js transpilation & watch tasks

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var babel = require('gulp-babel')
+var gulp = require('gulp')
+var plumber = require('gulp-plumber')
+
+var paths = {
+  js: 'lib/**/*.js'
+}
+
+gulp.task('js', function () {
+  return gulp.src(paths.js)
+    .pipe(plumber())
+    .pipe(babel({stage: 1}))
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('watch', function() {
+  gulp.watch(paths.js, ['js'])
+})
