5f8586e1df099ee583feea621c978d2b71c04911
Chris Wheatley <chriswheatley87@gmail.com>
2015-05-30 12:02:45 +0100

    explain gulp commands

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ var paths = {
   js: 'src/**/*.js'
 }
 
+/**
+ * transpile code & move to build dir
+ */
 gulp.task('js', function () {
   return gulp.src(paths.js)
     .pipe(plumber())
@@ -13,6 +16,9 @@ gulp.task('js', function () {
     .pipe(gulp.dest('build'))
 })
 
+/**
+ * watch for changes in src dir
+ */
 gulp.task('watch', function() {
   gulp.watch(paths.js, ['js'])
 })
